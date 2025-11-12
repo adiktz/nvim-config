bb277d0d9235f0569dcf8d7ddfdbacdd85943496
Gaurav Ravi <gauravraviji@gmail.com>
2025-11-13 00:35:18 +0530

    Fix telescope.nvim configuration error
    Remove deprecated cycle_history_next and cycle_history_previous actions
    that don't exist in current Telescope version

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -36,8 +36,6 @@ return {
 
           mappings = {
             i = {
-              ["<C-n>"] = actions.cycle_history_next,
-              ["<C-p>"] = actions.cycle_history_previous,
               ["<C-j>"] = actions.move_selection_next,
               ["<C-k>"] = actions.move_selection_previous,
               ["<C-c>"] = actions.close,
