7d90ef6583ac9527a1407c4036ffdd236a86ac3c
Gaurav Ravi <gauravraviji@gmail.com>
2025-11-14 00:20:28 +0530

    Fix Molten Jupyter image provider error
    - Change molten_image_provider from 'image.nvim' to 'none'
    - Removes dependency on image.nvim plugin
    - Jupyter notebooks will work for code execution
    - Images won't be rendered (simplifies setup)
    
    Fixes: Error 'image.nvim not found' when running :MoltenInit

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -62,7 +62,7 @@ return {
     init = function()
       -- Configuration
       vim.g.molten_auto_open_output = false
-      vim.g.molten_image_provider = "image.nvim"
+      vim.g.molten_image_provider = "none" -- Disable image rendering (no extra dependencies needed)
       vim.g.molten_output_win_max_height = 20
       vim.g.molten_wrap_output = true
       vim.g.molten_virt_text_output = true
