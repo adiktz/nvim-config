daf70926db843e6fb0a59e7acfb731cac603f2bf
Gaurav Ravi <gauravraviji@gmail.com>
2025-11-13 23:41:30 +0530

    Migrate LSP config to Neovim 0.11+ API
    - Replace deprecated lspconfig.setup() with vim.lsp.config()
    - Replace ruff_lsp with ruff (ruff_lsp is deprecated)
    - Migrate from on_attach to LspAttach autocmd
    - Add vim.lsp.enable() calls for all servers
    - Remove require('lspconfig') to avoid deprecation warnings
    
    Fixes deprecation warnings in Neovim 0.11+

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -24,7 +24,7 @@ return {
       require("mason-lspconfig").setup({
         ensure_installed = {
           "pyright",     -- Python
-          "ruff_lsp",    -- Python linting
+          "ruff",        -- Python linting (replaces ruff_lsp)
           "lua_ls",      -- Lua
         },
         automatic_installation = true,
@@ -40,7 +40,6 @@ return {
       "hrsh7th/cmp-nvim-lsp",
     },
     config = function()
-      local lspconfig = require("lspconfig")
       local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
       -- Add additional capabilities for autocompletion
@@ -66,38 +65,49 @@ return {
         vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
       end
 
-      -- LSP keymaps
-      local on_attach = function(client, bufnr)
-        local opts = { buffer = bufnr, silent = true }
+      -- LSP keymaps (applied via LspAttach autocmd)
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          local bufnr = args.buf
+          local opts = { buffer = bufnr, silent = true }
 
-        -- Navigation
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, vim.tbl_extend("force", opts, { desc = "Go to definition" }))
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, vim.tbl_extend("force", opts, { desc = "Go to declaration" }))
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, vim.tbl_extend("force", opts, { desc = "Go to implementation" }))
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, vim.tbl_extend("force", opts, { desc = "Show references" }))
+          -- Navigation
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, vim.tbl_extend("force", opts, { desc = "Go to definition" }))
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, vim.tbl_extend("force", opts, { desc = "Go to declaration" }))
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, vim.tbl_extend("force", opts, { desc = "Go to implementation" }))
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, vim.tbl_extend("force", opts, { desc = "Show references" }))
 
-        -- Hover and help
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_extend("force", opts, { desc = "Hover documentation" }))
-        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, vim.tbl_extend("force", opts, { desc = "Signature help" }))
+          -- Hover and help
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_extend("force", opts, { desc = "Hover documentation" }))
+          vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, vim.tbl_extend("force", opts, { desc = "Signature help" }))
 
-        -- Actions
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, vim.tbl_extend("force", opts, { desc = "Rename symbol" }))
-        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, vim.tbl_extend("force", opts, { desc = "Code action" }))
-        vim.keymap.set("n", "<leader>fm", function()
-          vim.lsp.buf.format({ async = true })
-        end, vim.tbl_extend("force", opts, { desc = "Format document" }))
+          -- Actions
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, vim.tbl_extend("force", opts, { desc = "Rename symbol" }))
+          vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, vim.tbl_extend("force", opts, { desc = "Code action" }))
+          vim.keymap.set("n", "<leader>fm", function()
+            vim.lsp.buf.format({ async = true })
+          end, vim.tbl_extend("force", opts, { desc = "Format document" }))
 
-        -- Diagnostics
-        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, vim.tbl_extend("force", opts, { desc = "Previous diagnostic" }))
-        vim.keymap.set("n", "]d", vim.diagnostic.goto_next, vim.tbl_extend("force", opts, { desc = "Next diagnostic" }))
-        vim.keymap.set("n", "<leader>dl", vim.diagnostic.open_float, vim.tbl_extend("force", opts, { desc = "Show diagnostic" }))
-        vim.keymap.set("n", "<leader>dq", vim.diagnostic.setloclist, vim.tbl_extend("force", opts, { desc = "Diagnostic list" }))
-      end
+          -- Diagnostics
+          vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, vim.tbl_extend("force", opts, { desc = "Previous diagnostic" }))
+          vim.keymap.set("n", "]d", vim.diagnostic.goto_next, vim.tbl_extend("force", opts, { desc = "Next diagnostic" }))
+          vim.keymap.set("n", "<leader>dl", vim.diagnostic.open_float, vim.tbl_extend("force", opts, { desc = "Show diagnostic" }))
+          vim.keymap.set("n", "<leader>dq", vim.diagnostic.setloclist, vim.tbl_extend("force", opts, { desc = "Diagnostic list" }))
 
-      -- Python LSP (Pyright)
-      lspconfig.pyright.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
+          -- Disable hover for Ruff in favor of Pyright
+          if client and client.name == "ruff" then
+            client.server_capabilities.hoverProvider = false
+          end
+        end,
+      })
+
+      -- Python LSP (Pyright) - Using new vim.lsp.config API
+      vim.lsp.config("pyright", {
+        cmd = { "pyright-langserver", "--stdio" },
+        filetypes = { "python" },
+        root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", ".git" },
         settings = {
           python = {
             analysis = {
@@ -108,22 +118,23 @@ return {
             },
           },
         },
+        capabilities = capabilities,
       })
 
-      -- Ruff LSP (Python linting)
-      lspconfig.ruff_lsp.setup({
+      -- Ruff LSP (Python linting) - Using new vim.lsp.config API
+      vim.lsp.config("ruff", {
+        cmd = { "ruff", "server" },
+        filetypes = { "python" },
+        root_markers = { "pyproject.toml", "ruff.toml", ".ruff.toml", ".git" },
+        settings = {},
         capabilities = capabilities,
-        on_attach = function(client, bufnr)
-          -- Disable hover in favor of Pyright
-          client.server_capabilities.hoverProvider = false
-          on_attach(client, bufnr)
-        end,
       })
 
-      -- Lua LSP
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
+      -- Lua LSP - Using new vim.lsp.config API
+      vim.lsp.config("lua_ls", {
+        cmd = { "lua-language-server" },
+        filetypes = { "lua" },
+        root_markers = { ".luarc.json", ".luarc.jsonc", ".luacheckrc", ".stylua.toml", "stylua.toml", "selene.toml", "selene.yml", ".git" },
         settings = {
           Lua = {
             diagnostics = {
@@ -138,7 +149,13 @@ return {
             },
           },
         },
+        capabilities = capabilities,
       })
+
+      -- Enable LSP servers for respective filetypes
+      vim.lsp.enable("pyright")
+      vim.lsp.enable("ruff")
+      vim.lsp.enable("lua_ls")
     end,
   },
 }
